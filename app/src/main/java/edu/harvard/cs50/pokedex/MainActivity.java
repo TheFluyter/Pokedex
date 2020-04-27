74dc1f96f4df6c84ca19e35622ae04824b9f247d
Rick de Ruiter <Ruiter@Ricks-iMac.local>
2020-04-27 21:55:55 +0200

    first commit

---
+++ app/src/main/java/edu/harvard/cs50/pokedex/MainActivity.java
@@ -0,0 +1,36 @@
+package edu.harvard.cs50.pokedex;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+    private RecyclerView recyclerView;
+    private PokedexAdapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        adapter = new PokedexAdapter(getApplicationContext());
+        layoutManager = new LinearLayoutManager(this);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(layoutManager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setOnQueryTextListener(this);
+
+        return true;
+    }
+}
